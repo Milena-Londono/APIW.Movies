f21e57452767abb56080e6f98936b46c629d65a4
Mile Londoño <Mile Londoo@MILU>
2025-11-23 17:53:38 -0500

    Add project files.

---
+++ APIW.Movies/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APIW.Movies.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIW.Movies.DAL
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        
+        }
+        //Sección para crear el dbset de las entidades o modelos
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ APIW.Movies/DAL/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIW.Movies.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required] //Este data annotation indica que el campo es obligatorio.
+        [Display(Name="Nombre de la Categoría")]//Me sirve para personalizar el nombre que se muestra en las vistas o mensajes de error
+        public string Name { get; set; }
+    }
+}
+
